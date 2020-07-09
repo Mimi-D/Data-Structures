89be8c627ba9ee9664394d447bc9f3a6f278a836
Mimi-D <67998464+Mimi-D@users.noreply.github.com>
2020-07-09 18:43:21 +0100

    Delete dsa.py

--- dsa.py
+++
@@ -1,9 +0,0 @@
-def letterToIndex(ch):
-    alphabet='abcdefghijklmnopqrstuvwxyz'
-    num=ord(ch.lower())
-    if num<97 or num>122:
-        print("INVALID INPUT")
-    else:
-        value=chr(num)
-        find=alphabet.find(value)
-        return find
